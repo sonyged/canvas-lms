1ba0905cc7fc40775852543836389848d34d5eab
quynk <quynk94@gmail.com>
2023-02-21 15:41:13 +0700

    add content scores column when download xlsx

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -1160,6 +1160,7 @@ class GradebooksController < ApplicationController
           'answer',
           'result',
           'duration',
+          'content scores',
         ]
         headers.each_with_index do |label, i|
           set_value(sheet, i, 0, label)
@@ -1201,11 +1202,13 @@ class GradebooksController < ApplicationController
 
               submission_for_question = submission.submission_data.select {|e| e.is_a?(Hash) && e[:question_id] == question['id'] }.first
               answer, result = get_answer_and_result(submission_for_question, question)
+              score = submission.score
 
               col_info += [
                 answer,
                 result,
-                duration
+                duration,
+                score
               ]
 
               col_info.each_with_index do |value, index|
